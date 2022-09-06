2e891232a2359809895613ab1dc432a8d745273b
Sven Riebe @ Dell <99394991+svenriebedell@users.noreply.github.com>
2022-09-06 12:24:10 +0200

    Update DellTrustedDevice_uninstall.ps1

--- DellTrustedDevice_uninstall.ps1
+++ DellTrustedDevice_uninstall.ps1
@@ -35,5 +35,6 @@ $ApplicationID_current = Get-CimInstance -ClassName Win32_Product -Filter "Name 
 #uninstall Software                                               #
 ###################################################################
 
-msiexec.exe /x $ApplicationID_current /qn
+Start-Process -FilePath msiexec.exe -ArgumentList "/x $ApplicationID_current /qn REBOOT=R" -Wait
 
+Exit 2
