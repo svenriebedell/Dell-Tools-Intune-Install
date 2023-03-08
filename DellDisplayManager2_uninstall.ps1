57a5f10d6b9d4b24ff8497336f2fa7555dd3c53e
Sven Riebe @ Dell <99394991+svenriebedell@users.noreply.github.com>
2023-03-08 15:16:32 +0100

    Create DellDisplayManager2_uninstall.ps1

---
+++ DellDisplayManager2_uninstall.ps1
@@ -0,0 +1,43 @@
+﻿<#
+_author_ = Sven Riebe <sven_riebe@Dell.com>
+_twitter_ = @SvenRiebe
+_version_ = 1.0
+_Dev_Status_ = Test
+Copyright Â© 2022 Dell Inc. or its subsidiaries. All Rights Reserved.
+
+No implied support and test in test environment/device before using in any production environment.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+    http://www.apache.org/licenses/LICENSE-2.0
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+#>
+
+<#
+.Synopsis
+   This PowerShell is for uninstall in Microsoft MEM for Dell Display Manager
+
+.DESCRIPTION
+   This PowerShell will uninstall a Device for Dell Display Manager. It will be used as install file in Microsoft MEM win32 install.
+   
+#>
+
+##### Variables
+$ApplicationID_current = Get-ChildItem -Path HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall | Get-ItemProperty | Where-Object {$_.DisplayName -match "Dell Display Manager" } | Select-Object -ExpandProperty UninstallString
+
+$ApplicationID_current = $ApplicationID_current
+
+
+###################################################################
+#uninstall Software                                               #
+###################################################################
+
+$IDProcess = Get-Process | Where-Object {$_.ProcessName -like 'ddm'} | Select-Object -ExpandProperty ID
+Stop-Process -Id $IDProcess -Force
+
+Start-Process $ApplicationID_current -ArgumentList '/S' -Wait
