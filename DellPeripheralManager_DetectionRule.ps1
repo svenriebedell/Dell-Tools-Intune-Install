0d7624aa13d24f4d07b5dd90eec5b2f1535c3782
Sven Riebe @ Dell <99394991+svenriebedell@users.noreply.github.com>
2023-05-16 13:50:42 +0200

    Create DellPeripheralManager_DetectionRule.ps1

---
+++ DellPeripheralManager_DetectionRule.ps1
@@ -0,0 +1,39 @@
+﻿<#
+_author_ = Sven Riebe <sven_riebe@Dell.com>
+_twitter_ = @SvenRiebe
+_version_ = 1.0
+_Dev_Status_ = Test
+Copyright © 2023 Dell Inc. or its subsidiaries. All Rights Reserved.
+
+No implied support and test in test environment/device before using in any production environment.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+    http://www.apache.org/licenses/LICENSE-2.0
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+#>
+
+<#
+.Synopsis
+   This PowerShell is for custom detection in Microsoft intune Dell Peripheral Manager
+
+.DESCRIPTION
+   This PowerShell will checki if Dell Peripheral Manager is ready installed on a device. It will be used as custom detection in Microsoft Intune win32 install.
+   
+#>
+
+######################################################################################################################
+# Program with target Version
+######################################################################################################################
+$ProgramVersion_target = '1.6.6' # need to be the same like the exe file
+[Version]$ProgramVersion_current = (Get-ItemProperty 'C:\Program Files\Dell\Dell Peripheral Manager\DPM.exe').VersionInfo | Select-Object -ExpandProperty ProductVersion
+
+if($ProgramVersion_current -eq $ProgramVersion_target)
+    {
+        Write-Host "Found it!"
+    }
