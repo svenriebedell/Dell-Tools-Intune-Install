ce934e26ea5eedad0d92ac1942f2cb55f6cf9c5b
Sven Riebe @ Dell <99394991+svenriebedell@users.noreply.github.com>
2023-10-26 16:37:21 +0200

    Update DellTrustedDevice_install.ps1

--- DellTrustedDevice_install.ps1
+++ DellTrustedDevice_install.ps1
@@ -1,7 +1,7 @@
 <#
 _author_ = Sven Riebe <sven_riebe@Dell.com>
 _twitter_ = @SvenRiebe
-_version_ = 1.1.0
+_version_ = 1.2.0
 _Dev_Status_ = Test
 Copyright (c)2023 Dell Inc. or its subsidiaries. All Rights Reserved.
 
@@ -22,6 +22,7 @@ limitations under the License.
     1.0.0     initial version
     1.1.0     add function get-msifileversion no more needed to edit var $ProgramVersion_target any more
               add MS EventLog LogName "Dell" Source "Dell Software Install" and "Dell Software Uninstall"
+    1.2.0     Installer migrated from MSI to DUP Format change script to run with the new format
 
 #>
 
@@ -61,154 +62,86 @@ function Get-installedcheck
 
     }
 
-#### function based on https://stackoverflow.com/questions/8743122/how-do-i-find-the-msi-product-version-number-using-powershell
-
-function Get-MsiFileVersion {
-    param 
-        (
-            [Parameter(mandatory=$true)][string] $msiName
-        )
-
-    try {
-        $FullPath = (Resolve-Path $msiName).Path
-        $windowsInstaller = New-Object -com WindowsInstaller.Installer
-
-        $database = $windowsInstaller.GetType().InvokeMember(
-                "OpenDatabase", "InvokeMethod", $Null, 
-                $windowsInstaller, @($FullPath, 0)
-            )
-
-        $q = "SELECT Value FROM Property WHERE Property = 'ProductVersion'"
-        $View = $database.GetType().InvokeMember(
-                "OpenView", "InvokeMethod", $Null, $database, ($q)
-            )
-
-        $View.GetType().InvokeMember("Execute", "InvokeMethod", $Null, $View, $Null)
-
-        $record = $View.GetType().InvokeMember(
-                "Fetch", "InvokeMethod", $Null, $View, $Null
-            )
-
-        [Version]$productVersion = $record.GetType().InvokeMember(
-                "StringData", "GetProperty", $Null, $record, 1
-            )
-
-        $View.GetType().InvokeMember("Close", "InvokeMethod", $Null, $View, $Null)
-
-        return $productVersion
-
-    } catch {
-        throw "Failed to get MSI file version the error was: {0}." -f $_
-    }
-}
-
 ##############################################
-#### Varibles section                     ####
+#### variable section                     ####
 ##############################################
-$InstallerName = Get-ChildItem .\*.msi | Select-Object -ExpandProperty Name
-$ProgramPath = (Get-Item .\$InstallerName).DirectoryName+ "\" + $InstallerName
-$ProgramVersion_target = Get-MsiFileVersion -msiName $InstallerName
+$InstallerName = Get-ChildItem .\*.exe | Select-Object -ExpandProperty Name
+$ProgramPath = ".\" + $InstallerName
+[Version]$ProgramVersion_target = (Get-Command $ProgramPath).FileVersionInfo.ProductVersion
 $AppSearch = "%Dell%Trusted%Device%" #Parameter to search in registry
-$Program_current = Get-CimInstance -ClassName Win32_Product -Filter "Name like '$AppSearch'"
-[Version]$ProgramVersion_current = $Program_current.Version
-$ArgumentString = '/i "'+$ProgramPath + '" /qn REBOOT=R'
-$SoftwareName = $Program_current.Name
-
+$UninstallApp = Get-CimInstance -ClassName Win32_Product | Where-Object {$_.Name -like "Dell*Trusted*Device*"}
+$SoftwareName = "Dell Trusted Device Agent"
+[Version]$ProgramVersion_current = $UninstallApp.Version
 
 ##############################################
-#### Program section                     ####
+#### program section                      ####
 ##############################################
 
 #### generate Logging Resources
 New-EventLog -LogName "Dell" -Source "Dell Software Install" -ErrorAction Ignore
 New-EventLog -LogName "Dell" -Source "Dell Software Uninstall" -ErrorAction Ignore
 
-##############################################
-#### Program section                      ####
-##############################################
+###################################################################
+#Checking if older Version is installed and uninstall this Version#
+###################################################################
 
-#Checking if older Version is installed and uninstall this Version
-If ($null -ne $ProgramVersion)
+If ($null -ne $ProgramVersion_current)
     {
 
     if ($ProgramVersion_target -gt $ProgramVersion_current)
         {
-        
-            #Update Software by msi upgrade      
-            Start-Process -FilePath msiexec.exe -ArgumentList "$ArgumentString" -Wait
+            #############################
+            # uninstall Software old    #
+            #############################
+            $Argument = "/x "+ $UninstallApp.PackageCache + " /qn REBOOT=ReallySuppress"
+            Start-Process -FilePath msiexec.exe -ArgumentList "$Argument" -Wait
+
 
             #############################
-            # install success check     #
+            # uninstall success check   #
             #############################
             $UninstallResult = Get-installedcheck -AppSearchString $AppSearch
-            [String]$VersionString = $ProgramVersion_target
-            $Program_current = Get-CimInstance -ClassName Win32_Product -Filter "Name like '$AppSearch'"
-            $SoftwareName = $Program_current.Name
 
-            If ($UninstallResult -ne $true)
-                                                            {
+            If ($UninstallResult -eq $true)
+                {
 
-                Write-Host "install is unsuccessful" -BackgroundColor Red
-            
-                $UninstallData = [PSCustomObject]@{
-                                                    Software = $SoftwareName
-                                                    Version = $VersionString
-                                                    Install = $false
-                                                    Reason = "Update failed"
-                                                  } | ConvertTo-Json
+                    Write-Host "uninstall is unsuccessful" -BackgroundColor Red
+
+                    $UninstallData = [PSCustomObject]@{
+                                          Software = $SoftwareName
+                                          Version = ($ProgramVersion_current).ToString()
+                                          Uninstall = $false
+                                     } | ConvertTo-Json
                 
-                Write-EventLog -LogName Dell -Source "Dell Software Install" -EntryType Error -EventId 11 -Message $UninstallData
+                    Write-EventLog -LogName Dell -Source "Dell Software Uninstall" -EntryType Error -EventId 11 -Message $UninstallData
 
-            }
+
+                }
             Else
-                                                                                                                                                {
+                {
 
-               [Version]$ProgramVersion_current = Get-CimInstance -ClassName Win32_Product -Filter "Name like '$AppSearch'" | Select-Object -ExpandProperty Version
+                    Write-Host "uninstall is successful" -BackgroundColor Green
 
-               If ($ProgramVersion_current -ge $ProgramVersion_target)
-                    {
-            
-                        Write-Host "install is successful" -BackgroundColor Green
-            
-                        $UninstallData = [PSCustomObject]@{
-                                                            Software = $SoftwareName
-                                                            Version = $VersionString
-                                                            Install = $true
-                                                            Reason = "Update"
-                                                          } | ConvertTo-Json
+                    $UninstallData = [PSCustomObject]@{
+                                          Software = $SoftwareName
+                                          Version = ($ProgramVersion_current).ToString()
+                                          Uninstall = $true
+                                     } | ConvertTo-Json
                 
-                       Write-EventLog -LogName Dell -Source "Dell Software Install" -EntryType Information -EventId 10 -Message $UninstallData
-                
-                    }
-               Else
-                    {
-                            
-            
-                       Write-Host "install is unsuccessful" -BackgroundColor red
+                    Write-EventLog -LogName Dell -Source "Dell Software Uninstall" -EntryType Information -EventId 10 -Message $UninstallData
 
-            
-                       $UninstallData = [PSCustomObject]@{
-                                                           Software = $SoftwareName
-                                                           Version = $VersionString
-                                                           Install = $false
-                                                           Reason = "Older Version installed $ProgramVersion_current"
-                                                         } | ConvertTo-Json
-                
-                       Write-EventLog -LogName Dell -Source "Dell Software Install" -EntryType Information -EventId 10 -Message $UninstallData
+                }
+        
 
-
-                    }
-
-            }
         }
-    
+
     Else
         {
             Write-Host "same version is installed"
 
             $UninstallData = [PSCustomObject]@{
                                       Software = $SoftwareName
-                                      Version = $ProgramVersion_current
+                                      Version = ($ProgramVersion_target).ToString()
                                       Install = $false
                                       Reason = "same version is installed"
                              } | ConvertTo-Json
@@ -220,77 +153,72 @@ If ($null -ne $ProgramVersion)
 
             Exit 0
         }
-
     }
 
+
+###################################################################
+#Install new Software                                             #
+###################################################################
+
+Start-Process -FilePath "$ProgramPath" -ArgumentList "/s" -Wait
+
+#############################
+# install success check     #
+#############################
+$UninstallResult = Get-installedcheck -AppSearchString $AppSearch
+
+If ($UninstallResult -ne $true)
+    {
+
+        Write-Host "install is unsuccessful" -BackgroundColor Red
+            
+        $UninstallData = [PSCustomObject]@{
+                                            Software = $SoftwareName
+                                            Version = ($ProgramVersion_target).ToString()
+                                            Install = $false
+                                            Reason = "Installation failed"
+                                          } | ConvertTo-Json
+                
+        Write-EventLog -LogName Dell -Source "Dell Software Install" -EntryType Error -EventId 11 -Message $UninstallData
+
+    }
 Else
     {
-    
-        #Install new Software
-        Start-Process -FilePath msiexec.exe -ArgumentList "$ArgumentString" -Wait
 
-        #############################
-        # install success check     #
-        #############################
-        $UninstallResult = Get-installedcheck -AppSearchString $AppSearch
-        $Program_current = Get-CimInstance -ClassName Win32_Product -Filter "Name like '$AppSearch'"
-        [String]$VersionString = $ProgramVersion_target
-        $SoftwareName = $Program_current.Name
+       [Version]$ProgramVersion_current = Get-CimInstance -ClassName Win32_Product -Filter "Name like '$AppSearch'" | Select-Object -ExpandProperty Version
 
-        If ($UninstallResult -ne $true)
+       If ($ProgramVersion_current -ge $ProgramVersion_target)
             {
-
-                Write-Host "install is unsuccessful" -BackgroundColor Red
+            
+                Write-Host "install is successful" -BackgroundColor Green
             
                 $UninstallData = [PSCustomObject]@{
-                                                    Software = "Trusted Device"
-                                                    Version = $VersionString
-                                                    Install = $false
-                                                    Reason = "Installation failed"
+                                                    Software = $SoftwareName
+                                                    Version = ($ProgramVersion_target).ToString()
+                                                    Install = $true
+                                                    Reason = "Update/Install/Newer Version"
                                                   } | ConvertTo-Json
                 
-                Write-EventLog -LogName Dell -Source "Dell Software Install" -EntryType Error -EventId 11 -Message $UninstallData
-
+               Write-EventLog -LogName Dell -Source "Dell Software Install" -EntryType Information -EventId 10 -Message $UninstallData
+                
             }
-        Else
+       Else
             {
-
-               [Version]$ProgramVersion_current = Get-CimInstance -ClassName Win32_Product -Filter "Name like '$AppSearch'" | Select-Object -ExpandProperty Version
-
-               If ($ProgramVersion_current -ge $ProgramVersion_target)
-                    {
-            
-                        Write-Host "install is successful" -BackgroundColor Green
-            
-                        $UninstallData = [PSCustomObject]@{
-                                                            Software = $SoftwareName
-                                                            Version = $VersionString
-                                                            Install = $true
-                                                            Reason = "Update/Install/Newer Version"
-                                                          } | ConvertTo-Json
-                
-                       Write-EventLog -LogName Dell -Source "Dell Software Install" -EntryType Information -EventId 10 -Message $UninstallData
-                
-                    }
-               Else
-                    {
                             
             
-                       Write-Host "install is unsuccessful" -BackgroundColor red
+               Write-Host "install is unsuccessful" -BackgroundColor red
 
             
-                       $UninstallData = [PSCustomObject]@{
-                                                           Software = $SoftwareName
-                                                           Version = $ProgramVersion_current
-                                                           Install = $false
-                                                           Reason = "Older Version installed $ProgramVersion_current"
-                                                         } | ConvertTo-Json
+               $UninstallData = [PSCustomObject]@{
+                                                   Software = $SoftwareName
+                                                   Version = ($ProgramVersion_target).ToString()
+                                                   Install = $false
+                                                   Reason = "Older Version installed $ProgramVersion_current"
+                                                 } | ConvertTo-Json
                 
-                       Write-EventLog -LogName Dell -Source "Dell Software Install" -EntryType Information -EventId 10 -Message $UninstallData
+               Write-EventLog -LogName Dell -Source "Dell Software Install" -EntryType Information -EventId 10 -Message $UninstallData
 
-
-                    }
 
             }
-    
+
     }
