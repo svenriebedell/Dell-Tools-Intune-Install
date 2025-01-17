45f8238bc3a1d16edf07b5f488156ab7d43b5eaf
Sven Riebe @ Dell <99394991+svenriebedell@users.noreply.github.com>
2025-01-17 11:44:58 +0100

    Create DellCommandEndpointConfigureIntune_DetectionRule.ps1

---
+++ DellCommandEndpointConfigureIntune_DetectionRule.ps1
@@ -0,0 +1,39 @@
+﻿<#
+_author_ = Sven Riebe <sven_riebe@Dell.com>
+_twitter_ = @SvenRiebe
+_version_ = 1.0
+_Dev_Status_ = Test
+Copyright (C) 2025 Dell Inc. or its subsidiaries. All Rights Reserved.
+
+No implied support and test in test environment/device before using in any production environment.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+    http://www.apache.org/licenses/LICENSE-2.0
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+#>
+
+<#
+.Synopsis
+   This PowerShell is for custom detection in Microsoft MEM for Dell Command | Configure
+
+.DESCRIPTION
+   This PowerShell will checki if Dell Command | Configure is ready installed on a device. It will be used as custom detection in Microsoft MEM win32 install.
+   
+#>
+
+######################################################################################################################
+# Program with target Version
+######################################################################################################################
+$ProgramVersion_target = '2.0.0.6' # need to be the same like the exe file
+$ProgramVersion_current = Get-CimInstance -ClassName Win32_Product -Filter "Name like '%Dell%Endpoint%'" | Select-Object -ExpandProperty Version
+
+if($ProgramVersion_current -ge $ProgramVersion_target)
+    {
+        Write-Host "Found it!"
+    }
